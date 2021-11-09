376325b571d536e78c83a251ce981343e637011b
gfalco <gfalco@edt>
2021-11-09 20:57:45 +0100

    ..

---
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.model.File;
+import com.example.demo.repository.FileRepository;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/files")
+public class FileController {
+
+    private final FileRepository fileRepository;
+    FileController(FileRepository fileRepository){
+        this.fileRepository = fileRepository;
+    }
+
+    @PostMapping
+    public String upload(@RequestParam("file") MultipartFile uploadedFile) {
+        try {
+            File file = new File();
+            file.contenttype = uploadedFile.getContentType();
+            file.data = uploadedFile.getBytes();
+
+            return fileRepository.save(file).fileid.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<byte[]> getFile(@PathVariable UUID id) {
+        File file = fileRepository.findById(id).orElse(null);
+
+        if (file == null) return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(file.contenttype))
+                .contentLength(file.data.length)
+                .body(file.data);
+    }
+}
